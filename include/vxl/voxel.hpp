de079a6c96b90acc4970e28ed71c05da5765ff62
Nikita Fediuchin <thequantumbranch@gmail.com>
2020-04-25 19:55:55 +0300

    Remove submodules

---
+++ include/vxl/voxel.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <cstdint>
+
+namespace vxl
+{
+	typedef uint8_t voxel8_t;
+	typedef uint16_t voxel16_t;
+	typedef uint32_t voxel32_t;
+	typedef uint64_t voxel64_t;
+
+	struct VoxelInfo
+	{
+
+	};
+}
